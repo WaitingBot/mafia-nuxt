005b1aca9b43ac01047f2dd9d16016c9a21c442b
WaitingBot <waitingbot@gmail.com>
2020-07-11 13:21:50 +0700

    shuffle deck

--- components/CardItem.vue
+++ components/CardItem.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="card">
+  <div class="card" :class="classFromName(name) ">
     <div class="name">
-      тест
+      {{name}}
+      {{user_num}}
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "CardItem"
+  name: "CardItem",
+  props: {
+    name: String,
+    user_num: Number
+  },
+
+  methods: {
+    classFromName(nameCl) {
+      if (nameCl === 'Мафия') return 'mafia'
+      return ''
+    }
+  }
 }
 </script>
 
@@ -19,12 +31,19 @@ export default {
   height: 175px;
   background-color: #BFBFBF;
   border-radius: 15px;
+  display: inline-block;
+  margin: 10px 10px;
 }
 
 .name {
   text-align: center;
   vertical-align: middle;
   line-height: 175px;
+}
+
+.mafia {
+  background-color: darkred;
+  color: #ffffff;
 }
 
 </style>
--- components/CardList.vue
+++ components/CardList.vue
@@ -1,15 +1,69 @@
 <template>
   <div class="card-list">
-    <CardItem />
+    <CardItem v-for="(card, index) in deck"
+              :key="index"
+              :name="card.name"
+              :user_num="card.user_num"
+    />
+
+    <b-button @click="generateDeck">click</b-button>
   </div>
 </template>
 
 <script>
 import CardItem from './CardItem';
 
+function shuffle(arr){
+  let j, temp;
+  for(let i = arr.length - 1; i > 0; i--){
+    j = Math.floor(Math.random()*(i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
 export default {
   name: "CardList",
-  components: {CardItem}
+  components: {CardItem},
+
+  data() {
+    return {
+      deck: []
+    }
+  },
+
+  methods: {
+    generateDeck() {
+      const allCards = this.$store.state.roles
+      let newDeck = []
+      allCards.forEach(role => {
+        for (let i = 0; i< role.number; i++) {
+          newDeck.push({
+            name: role.name,
+            user_num: null
+          })
+        }
+      })
+
+      newDeck = shuffle(newDeck)
+
+      if (newDeck.length === this.deck.length) {
+        for (let i = 0; i< newDeck.length; i++) {
+          newDeck[i].user_num = this.deck[i].user_num
+        }
+      } else {
+        let number = 1
+        for (let i = 0; i< newDeck.length; i++) {
+          newDeck[i].user_num = number
+          number += 1
+        }
+      }
+
+      this.deck = newDeck
+    }
+  }
 }
 </script>
 
